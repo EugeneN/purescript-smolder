d125762b59620c21fc063f5d96c419568b7907f9
EugeneN <eugene.naumenko@gmail.com>
2016-01-24 11:41:54 +0200

    Added HtmlEntity 2

--- src/Text/Smolder/Renderer/Util.purs
+++ src/Text/Smolder/Renderer/Util.purs
@@ -15,13 +15,15 @@ import qualified Text.Smolder.Markup as Markup
 data Node
   = Element String (Map.Map String String) (List Node)
   | Text String
+  | HtmlEntity String
 
 renderMarkup :: forall a. Markup.MarkupM a -> List Node
-renderMarkup (Markup.Element name (Just children) attrs rest) = 
+renderMarkup (Markup.Element name (Just children) attrs rest) =
   Element name (renderAttrs attrs) (renderMarkup children) : renderMarkup rest
-renderMarkup (Markup.Element name Nothing attrs rest) = 
+renderMarkup (Markup.Element name Nothing attrs rest) =
   Element name (renderAttrs attrs) Nil : renderMarkup rest
 renderMarkup (Markup.Content text rest) = Text text : renderMarkup rest
+renderMarkup (Markup.HtmlEntity text rest) = HtmlEntity text : renderMarkup rest
 renderMarkup (Markup.Return _) = Nil
 
 renderAttrs :: Array Markup.Attr -> Map.Map String String
